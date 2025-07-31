b18b8e69ee32e94d8038357360db1138aa26eb12
Caio Guerra <125095125+CaioSGuerra@users.noreply.github.com>
2025-07-31 14:42:19 -0300

    assign piece images and render on board

---
+++ ChessUI/Images.cs
@@ -0,0 +1,61 @@
+﻿using ChessLogic.ChessPiece;
+using ChessLogic.Enum;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ChessUI
+{
+    //This class is to load pieces from Assets folder and access them
+    public static class Images
+    {
+        //Adding white pieces 
+        private static readonly Dictionary<PieceType, ImageSource> whiteSources = new()
+        {
+            {PieceType.Pawn, LoadImage("Assets/PawnW.png") },
+            {PieceType.Bishop, LoadImage("Assets/BishopW.png") },
+            {PieceType.Knight, LoadImage("Assets/KnightW.png") },
+            {PieceType.Rook, LoadImage("Assets/RookW.png") },
+            {PieceType.Queen, LoadImage("Assets/QueenW.png") },
+            {PieceType.King, LoadImage("Assets/KingW.png") },
+        };
+
+        //Adding black pieces
+        private static readonly Dictionary<PieceType, ImageSource> blackSources = new()
+        {
+            {PieceType.Pawn, LoadImage("Assets/PawnB.png") },
+            {PieceType.Bishop, LoadImage("Assets/BishopB.png") },
+            {PieceType.Knight, LoadImage("Assets/KnightB.png") },
+            {PieceType.Rook, LoadImage("Assets/RookB.png") },
+            {PieceType.Queen, LoadImage("Assets/QueenB.png") },
+            {PieceType.King, LoadImage("Assets/KingB.png") },
+        };
+
+        //To load a Image
+        private static ImageSource LoadImage(string filePath)
+        {
+            return new BitmapImage(new Uri(filePath, UriKind.Relative));
+        }
+
+        //This get the color and piece type and show the correspond image
+        public static ImageSource GetImage(Player color, PieceType type)
+        {
+            return color switch
+            {
+                Player.White => whiteSources[type],
+                Player.Black => blackSources[type],
+                _ => null
+            };
+        }
+
+        //Overload of 'GetImage" which takes a piece
+        public static ImageSource GetImage(Piece piece)
+        {
+            if (piece == null)
+            {
+                return null;
+            }
+
+            return GetImage(piece.Color, piece.Type);
+        }
+    }
+}
--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using ChessLogic;
+using ChessLogic.ChessPiece;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChessUI
 {
@@ -16,9 +10,52 @@ namespace ChessUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Image[,] pieceImages = new Image[8, 8];
+
+        private GameState gameState;
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+
+            gameState = new GameState(ChessLogic.Enum.Player.White, Board.Initial());
+            DrawBoard(gameState.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            //Matrix for positions
+            for (int row = 0; row < 8; row++)
+            {
+                for (int column = 0; column < 8; column++)
+                {
+                    //Creating a image control
+                    Image image = new Image();
+
+                    //store in a 2d array
+                    pieceImages[row, column] = image;
+
+                    //add as a child to uniform grid
+                    PieceGrid.Children.Add(image);
+
+                }
+            }
+        }
+
+        //This set the source of all image controls so they match the pieces on that board
+        private void DrawBoard(Board board)
+        {
+            //Matrix for positions
+            for (int row = 0; row < 8; row++)
+            {
+                for (int column = 0; column < 8; column++)
+                {
+                    //Take the piece of the position
+                    Piece piece = board[row, column];
+                    //Update the source of correspond image control
+                    pieceImages[row, column].Source = Images.GetImage(piece);
+                }
+            }
         }
     }
 }
