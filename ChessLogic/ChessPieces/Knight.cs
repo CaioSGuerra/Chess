7a7b0437fd497e1a5965ae4e70736fd9bfd61895
Caio Guerra <125095125+CaioSGuerra@users.noreply.github.com>
2025-08-11 12:05:41 -0300

    Create move logic for Knight

--- ChessLogic/ChessPieces/Knight.cs
+++ ChessLogic/ChessPieces/Knight.cs
@@ -1,4 +1,5 @@
 ﻿using ChessLogic.Enum;
+using ChessLogic.Moves;
 
 namespace ChessLogic.ChessPiece
 {
@@ -17,6 +18,34 @@ namespace ChessLogic.ChessPiece
             Knight copy = new Knight(Color);
             copy.HasMoved = HasMoved;
             return copy;
+        }
+
+        // A help method to "jump" pieces, it show's all potential moves ignoring all pieces on the way
+        private static IEnumerable<Position> PotentialToPositions(Position fromPosition)
+        {
+            // All vertical positions
+            foreach (Direction verticalDirection in new Direction[] { Direction.North, Direction.South })
+            {
+                // All horizontal positions
+                foreach (Direction horizontalDirection in new Direction[] { Direction.West, Direction.East })
+                {
+                    yield return fromPosition + 2 * verticalDirection + horizontalDirection;
+                    yield return fromPosition + 2 * verticalDirection + horizontalDirection;
+                }
+            }
+        }
+
+        // a method to return all positions knight can actually move
+        private IEnumerable<Position> MovePositions(Position fromPosition, Board board)
+        {
+            return PotentialToPositions(fromPosition).Where(position => Board.IsInside(position)
+            && (board.IsEmpty(position) || board[position].Color != Color));
+        }
+
+        // The Method to move the piece
+        public override IEnumerable<Move> GetMoves(Position fromPosition, Board board)
+        {
+            return MovePositions(fromPosition, board).Select(toPosition => new NormalMove(fromPosition, toPosition));
         }
     }
 }
