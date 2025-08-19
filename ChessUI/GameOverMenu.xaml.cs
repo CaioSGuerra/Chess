b8a890bf02469005241266e81dad1670b79b54aa
Caio Guerra <125095125+CaioSGuerra@users.noreply.github.com>
2025-08-18 23:32:22 -0300

    Create game over menu

---
+++ ChessUI/GameOverMenu.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessUI
+{
+    /// <summary>
+    /// Interaction logic for GameOver.xaml
+    /// </summary>
+    public partial class GameOver : UserControl
+    {
+        public GameOver()
+        {
+            InitializeComponent();
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
